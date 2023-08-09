2191a9735ea4d07625cf105a54aa2af5ddd9b156
Juliette Vila <contactjuliettevila@gmail.com>
2023-08-09 15:45:21 +0200

    📦 NEW:  affichage des articles et mise en page

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+//afficher la liste de nos articles
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
+
+if (file_exists($filename)) {
+  $articles = json_decode(file_get_contents($filename), true) ?? [];
+}
+//echo count($articles);
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -11,6 +22,17 @@
   <div class="container">
     <?php require_once 'includes/header.php' ?>
     <div class="content">
+      <div class="articles-container">
+        <?php foreach ($articles as $a) : ?>
+        <div class="article block">
+              <div class="overflow">
+                <div class="img-container" style="background-image:url(<?= $a['image'] ?>">
+                </div>
+                </div>
+                <h2><?= $a['title'] ?></h2>
+              </div>
+            <?php endforeach; ?>
+          </div>
     </div>
     <?php require_once 'includes/footer.php' ?>
   </div>
